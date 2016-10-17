9160abaa0c1afcae383f8d8cd2dfe56a4dea4c15
SungHyun Ryu <sunghyun.ryu@navercorp.com>
2016-10-17 14:49:30 +0900

    project »ý¼º

---
+++ src/main/java/org/bucketmap/BucketMapApplication.java
@@ -0,0 +1,12 @@
+package org.bucketmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BucketMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BucketMapApplication.class, args);
+	}
+}
